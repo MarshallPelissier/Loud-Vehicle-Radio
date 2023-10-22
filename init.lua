1e178bd2fdcb934468acfc4770bca985223919d2
mar <marshallpelissier@gmail.com>
2023-10-21 22:25:56 -0700

    Moved audio logic to new module. Added support for increased volume

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 local LoudVehicleRadio = { version = "1.0.0" }
 local Cron = require("Modules/Cron")
+local audio = require("Modules/audio")
 
 VehicleStationList = {
     "Gameplay-Devices-Radio-RadioStationAggroIndie",
@@ -18,41 +19,19 @@ VehicleStationList = {
     "Gameplay-Devices-Radio-RadioStationImpulseFM",
 }
 
-SpeakerStationList = {
-    "radio_station_02_aggro_ind",
-    "radio_station_03_elec_ind",
-    "radio_station_04_hiphop",
-    "radio_station_07_aggro_techno",
-    "radio_station_09_downtempo",
-    "radio_station_01_att_rock",
-    "radio_station_05_pop",
-    "radio_station_10_latino",
-    "radio_station_11_metal",
-    "radio_station_06_minim_techno",
-    "radio_station_08_jazz",
-    "radio_station_12_growl_fm",
-    "radio_station_13_dark_star",
-    "radio_station_14_impulse_fm",
-}
-
-Speaker = {
-    path = "base\\gameplay\\devices\\home_appliances\\radio_sets\\speaker_virtual.ent",
-    entID = nil,
-    entity = nil,
-    active = false
-}
-
 Vehicle = {
     base = nil,
     record = nil,
     station = nil,
     lastLoc = nil,
-    mounted = false
+    mounted = false,
+    entering = false
 }
 
 Checks = {
     count = 0,
-    spawned = false
+    spawned = false,
+    startup = true
 }
 
 local timer = nil
@@ -60,116 +39,68 @@ local rot = nil
 
 LoudVehicleRadio = {}
 
-function LoudVehicleRadio:New()
-    registerForEvent("onInit", function()
-        rot = EulerAngles.new(0,90,0)
-        Observe('hudCarController', 'OnMountingEvent', function()
-            OnVehicleEntered()
-        end)
-
-        Observe('hudCarController', 'OnUnmountingEvent', function()
-            OnVehicleExited()
-        end)
-
-        Observe('LoadGameMenuGameController', 'OnUninitialize', function()
-            Cleanup()
-        end)
-        
-        if HasMountedVehicle() then
-            GetVehicleData()
-        end
-    end)
-
-    registerForEvent("onUpdate", function(delta)
-        Cron.Update(delta)
-    end)
-
-
-    registerForEvent("onShutdown", function(delta)
-        Cleanup()
-    end)
-
-    timer = Cron.Every(.1, Update)
-
-    return {
-      version = LoudVehicleRadio.version
-    }
-end
-
 function Cleanup()
-    Despawn()
+    audio.Despawn()
 
     Vehicle.base = nil
     Vehicle.record = nil
     Vehicle.station = nil
     Vehicle.lastLoc = nil
     Vehicle.mounted = false
+    Vehicle.entering = false
 
     Checks.count = 0
+    Checks.spawned = true
+    Checks.startup = true
 end
 
-function Update()
-    if Vehicle.base ~= nil then
-        if Speaker.entity == nil then
-            if Checks.spawned then
-                SetSpeaker()
-            else
-                Spawn()
-            end
-        else
-            -- Check if vehicle radio station has changed
-            if StationChanged() then
-                Vehicle.station = GetVehicleStation()
-                Speaker.active = false
-                SetSpeaker()
-            end
+function IsEnteringVehicle()
+    return IsInVehicle() and Game.GetWorkspotSystem():GetExtendedInfo(Game.GetPlayer()).entering
+end
+function IsExitingVehicle()
+    return IsInVehicle() and Game.GetWorkspotSystem():GetExtendedInfo(Game.GetPlayer()).exiting
+end
 
-            -- moves speaker with vehicle until it comes to a full stop
-            local pos = Vehicle.base:GetWorldTransform().Position
-            if VectorCompare(pos, Vehicle.lastLoc) then
-                if Vehicle.mounted == false then
-                    Checks.count = Checks.count + 1
-                end
-            else
-                Game.GetTeleportationFacility():Teleport(Speaker.entity, VectorFromPosition(pos) , rot)
-                Vehicle.lastLoc = pos
-                Checks.count = 0
-            end
-        end
-    else
-        Cron.Pause(timer)
-    end
-
-    -- stops the timer if the car has been exited, the car has come to a stop, and the speaker is already spawned
-    if Speaker.entID ~= nil and Checks.count > 4 then
-        Cron.Pause(timer)
-        Checks.count = 0
-    end
+function IsInVehicle()
+    local player = Game.GetPlayer()
+    return player and Game.GetWorkspotSystem():IsActorInWorkspot(player)
+            and Game.GetWorkspotSystem():GetExtendedInfo(player).isActive
+            and HasMountedVehicle()
+            and IsPlayerDriver()
 end
 
 function OnVehicleEntered()
-    Vehicle.mounted = true
-    Checks.count = 0
+    print("ENTERED VEHICLE ---")
     if Vehicle.record ~= GetMountedVehicleRecord() then
         GetVehicleData()
-        Despawn()
     end
+    Vehicle.mounted = true
+    Checks.count = 0
     Cron.Resume(timer)
 end
 
+function OnVehicleEntering()
+    print("ENTERING VEHICLE")
+end
+
+function OnVehicleExiting()
+    print("EXITING VEHICLE")
+end
+
 function OnVehicleExited()
+    print("EXITED VEHICLE ---")
     Vehicle.mounted = false
 end
 
--- function PrintPosition(pos)
---     local x = pos:GetX()
---     local y = pos:GetY()
---     local z = pos:GetZ()
---     print("X: " .. x .. " - Y: " .. y .. " - Z: " .. z)
--- end
-
 function HasMountedVehicle()
     return not not Game['GetMountedVehicle;GameObject'](Game.GetPlayer())
+end
+
+function IsPlayerDriver()
+    local veh = Game['GetMountedVehicle;GameObject'](Game.GetPlayer())
+    if veh then
+        return veh:IsPlayerDriver()
+    end
 end
 
 function GetMountedVehicleRecord()
@@ -198,32 +129,34 @@ function StationChanged()
     return Vehicle.station ~= station
 end
 
-function SetSpeaker()
-    if Game.FindEntityByID(Speaker.entID) ~= nil then
-        Speaker.entity = Game.FindEntityByID(Speaker.entID)
-        Speaker.entity:GetDevicePS():SetCurrentStation(SpeakerStationList[Vehicle.station])
-        if not Speaker.active then
-            Speaker.entity:TurnOnDevice()
-            Speaker.active = true
-        end
-    end
-end
+-- function SetSpeaker()
+--     if Game.FindEntityByID(Speaker.entID) ~= nil then
+--         Speaker.entity = Game.FindEntityByID(Speaker.entID)
+--         Speaker.entity:GetDevicePS():SetCurrentStation(SpeakerStationList[Vehicle.station])
+--         if not Speaker.active then
+--             Speaker.entity:TurnOnDevice()
+--             Speaker.active = true
+--         end
+--     end
+-- end
 
-function Spawn()
-    local transform = Vehicle.base:GetWorldTransform()
-    Speaker.entID = exEntitySpawner.Spawn(Speaker.path, transform)
-    Checks.spawned = true
-end
+-- function Spawn()
+--     print("Spawn Speaker")
+--     local transform = Vehicle.base:GetWorldTransform()
+--     Speaker.entID = exEntitySpawner.Spawn(Speaker.path, transform)
+--     Checks.spawned = true
+-- end
 
-function Despawn()
-    if Game.FindEntityByID(Speaker.entID) ~= nil then
-        Game.FindEntityByID(Speaker.entID):GetEntity():Destroy()
-        Checks.spawned = false
-        Speaker.entID = nil
-        Speaker.entity = nil
-        Speaker.active = false
-    end
-end
+-- function Despawn()
+--     if Game.FindEntityByID(Speaker.entID) ~= nil then
+--         print("Despawn Speaker")
+--         Game.FindEntityByID(Speaker.entID):GetEntity():Destroy()
+--         Checks.spawned = false
+--         Speaker.entID = nil
+--         Speaker.entity = nil
+--         Speaker.active = false
+--     end
+-- end
 
 function VectorFromPosition(pos)
     return Vector4.new(pos:GetX(),pos:GetY(),pos:GetZ())
@@ -251,6 +184,113 @@ function IndexOf(array, value)
             return i
         end
     end
+end
+
+-- function PrintPosition(pos)
+--     local x = pos:GetX()
+--     local y = pos:GetY()
+--     local z = pos:GetZ()
+--     print("X: " .. x .. " - Y: " .. y .. " - Z: " .. z)
+-- end
+
+function Update()
+    if HasMountedVehicle() and Vehicle.base == nil then
+        GetVehicleData()
+    end
+
+    if IsEnteringVehicle() then
+        OnVehicleEntering()
+    elseif IsExitingVehicle() then
+        OnVehicleExiting()
+    end
+
+    if Vehicle.base ~= nil then
+        if not audio.ready and IsExitingVehicle() then
+            if audio.spawned then
+                audio.SetSpeaker(Vehicle.station)
+            else
+                audio.SpawnAll(Vehicle.base:GetWorldTransform())
+            end
+        elseif audio.ready then
+            -- Check if vehicle radio station has changed
+            if StationChanged() then
+                Vehicle.station = GetVehicleStation()
+                -- Speaker.active = false
+                audio.SetSpeaker(Vehicle.station)
+            end
+
+            -- moves speaker with vehicle until it comes to a full stop
+            local pos = Vehicle.base:GetWorldTransform().Position
+            if VectorCompare(pos, Vehicle.lastLoc) then
+                if Vehicle.mounted == false then
+                    Checks.count = Checks.count + 1
+                end
+            else
+                audio.Teleport(pos, rot)
+                Vehicle.lastLoc = pos
+                Checks.count = 0
+            end
+
+            if Vehicle.entering and not IsEnteringVehicle() then
+                Vehicle.entering = false
+                audio.counter = 1
+            end
+            
+            if audio.counter == audio.despawnDelay then
+                audio.counter = 0
+                audio.Despawn()
+            elseif audio.counter > 0 then
+                audio.counter = audio.counter + 1
+            end
+
+            Vehicle.entering = IsEnteringVehicle()
+        end
+    else
+        print("Pause")
+        Cron.Pause(timer)
+    end
+
+    -- stops the timer if the car has been exited, the car has come to a stop, and the speaker is already spawned
+    if audio.spawned and Checks.count > 4 then
+        Cron.Pause(timer)
+        Checks.count = 0
+    end
+
+end
+
+function LoudVehicleRadio:New()
+    registerForEvent("onInit", function()
+        rot = EulerAngles.new(0,90,0)
+        Observe('hudCarController', 'OnMountingEvent', function()
+            OnVehicleEntered()
+        end)
+
+        Observe('hudCarController', 'OnUnmountingEvent', function()
+            OnVehicleExited()
+        end)
+
+        Observe('LoadGameMenuGameController', 'OnUninitialize', function()
+            Cleanup()
+        end)
+        
+        if HasMountedVehicle() then
+            GetVehicleData()
+        end
+    end)
+
+    registerForEvent("onUpdate", function(delta)
+        Cron.Update(delta)
+    end)
+
+    registerForEvent("onShutdown", function()
+        Cleanup()
+    end)
+
+    timer = Cron.Every(.1, Update)
+
+    return {
+      version = LoudVehicleRadio.version
+    }
 end
 
 return LoudVehicleRadio:New()
